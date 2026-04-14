dd644e500481b2e330433ad78f844a9500e50248
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-04-14 17:14:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'ඔයාගේ_ID_එක',
-    MONGODB: process.env.MONGODB || 'ඔයාගේ_මොන්ගෝ_ලින්ක්_එක', // දත්ත සේව් කරන්න
+    MONGODB: process.env.MONGODB || 'mongodb+srv://fcbsofficial2223_db_user:V0N72r9PrFb9gBBz@cluster0.rxntu0s.mongodb.net/?appName=Cluster0', // දත්ත සේව් කරන්න
     GEMINI_API: process.env.GEMINI_API || 'AIzaSyBBNVFNbqAw5pVyrgMMb2lsUo_Mk5f-TxI', // AI එකට
     OWNER_NAME: "Digi Solutions",
     BOT_NAME: "DigiSolutions-MD",
