0e7b33b9a380c175bdcf0998d3a40c27816642e3
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-04-14 17:10:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'ඔයාගේ_ID_එක',
     MONGODB: process.env.MONGODB || 'ඔයාගේ_මොන්ගෝ_ලින්ක්_එක', // දත්ත සේව් කරන්න
-    GEMINI_API: process.env.GEMINI_API || 'ඔයාගේ_GEMINI_KEY_එක', // AI එකට
+    GEMINI_API: process.env.GEMINI_API || 'AIzaSyBBNVFNbqAw5pVyrgMMb2lsUo_Mk5f-TxI', // AI එකට
     OWNER_NAME: "Digi Solutions",
     BOT_NAME: "DigiSolutions-MD",
     PREFIX: "."
