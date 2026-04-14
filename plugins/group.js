4f26e7c483f8f978d9d127f2d2094953740035e2
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-04-14 17:03:28 +0530

    Delete plugins/group.js

--- plugins/group.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (conn, from, body, msg) => {
-    if (body === '.tagall') {
-        const groupMetadata = await conn.groupMetadata(from);
-        const participants = groupMetadata.participants;
-        let txt = `📢 *Attention Everyone!*\n\n`;
-        for (let mem of participants) { txt += ` @${mem.id.split('@')[0]}`; }
-        await conn.sendMessage(from, { text: txt, mentions: participants.map(a => a.id) });
-    }
-};
