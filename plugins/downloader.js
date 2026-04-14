fd2ab3b1be8091092ccacf75102d5ac07e1b0997
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-04-14 17:03:18 +0530

    Delete plugins/downloader.js

--- plugins/downloader.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (conn, from, command, body) => {
-    if (command.startsWith('.fb ') || command.startsWith('.yt ')) {
-        const link = body.slice(4);
-        await conn.sendMessage(from, { text: `📥 *Downloading...*\n\nඔබ ලබාදුන් ලින්ක් එක පරීක්ෂා කරමින් පවතී: ${link}` });
-    }
-};
